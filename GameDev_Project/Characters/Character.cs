fd4de0eb7adf8f1a4861cccd7c8b078b2c95213e
Bas De Meurichy <bas.demeurichy@gmail.com>
2024-12-03 10:49:07 +0100

    Create new map

--- GameDev_Project/Characters/Character.cs
+++ GameDev_Project/Characters/Character.cs
@@ -33,7 +33,7 @@ namespace GameDev_Project.Characters
             health = 1;
             maxHealth = health;
             dead = false;
-            gravity = 2f;
+            gravity = 1f;
         }
 
         //TODO: Use GetHit when player gets hit by enemy or trap ...
