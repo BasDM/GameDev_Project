a8ae6f7d38c35dcac600ac35573bc03e4ac1b5ee
BasDM <bas.demeurichy@gmail.com>
2024-12-28 14:23:05 +0100

    Remove coin class and collectable interface. Going to implement it in another way

--- GameDev_Project/Interfaces/ICollectable.cs
+++
@@ -1,9 +0,0 @@
-﻿using GameDev_Project.Characters;
-
-namespace GameDev_Project.Interfaces
-{
-    public interface ICollectable : IAreaGameComponent
-    {
-        void Collect(Hero hero);
-    }
-}
